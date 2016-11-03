50ae2f782f7295335754ed89c67ee244111a4118
dougwells <dougwells@gmail.com>
2016-11-03 08:16:03 -0600

    Swift. Conditionals and Loops

---
+++ conditionalsAndLoops.playground/Contents.swift
@@ -0,0 +1,17 @@
+let height = 50
+let changeWidth = false
+
+let width: Int
+
+//Conditional
+if changeWidth {
+    width = 50
+}else{
+    width = 40
+}
+
+//Loop
+
+for index in 1...10 {
+    print(index*5)
+}
---
+++ swiftPlayTypes.playground/Contents.swift
@@ -0,0 +1,16 @@
+// cannot add vars of different types
+// can convert var type during operation
+var num1 = 3
+var num2 = 0.14
+
+var pi = Double(num1) + num2
+
+typealias Str = String
+
+var test: Str
+
+test = "Hello"
+
+print(test, "World")
+
+
