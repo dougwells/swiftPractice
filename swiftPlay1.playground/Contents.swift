cf359080ce31ac8ba914dac64f95e548abcb9097
dougwells <dougwells@gmail.com>
2016-11-03 07:39:18 -0600

    Learning Swift. Variables and Constants

---
+++ swiftPlay1.playground/Contents.swift
@@ -0,0 +1,15 @@
+var str = "Hello, playground"
+
+var num = 10
+
+var x=10, y=11, z=12.2
+
+var a = 5
+
+var welcome = "Welcome to Swift"
+
+var test: String
+
+test = "Hello"
+
+print(test, " World")
