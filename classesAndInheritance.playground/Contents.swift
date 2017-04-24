5b8cc86ca645c8bc84277d941871c0ee74a34d84
dougwells <dougwells@gmail.com>
2017-04-24 11:31:42 -0700

    Swift Practice. Classes and inheritance.

--- classesAndInheritance.playground/Contents.swift
+++ classesAndInheritance.playground/Contents.swift
@@ -1,5 +1,56 @@
-//: Playground - noun: a place where people can play
+/* Classes & Inheritance: https://www.makeschool.com/online-courses/tutorials/learn-swift-by-example-pt-3-classes-initialization
+ 
+*/
 
-import UIKit
+class User {
+    
+    let name: String
+    let location: String
+    let age: Int
+    
+    func greeting() {print("I'm \(name) from \(location)")}
+    
+    init(name: String, location: String, age: Int) {
+        self.name = name
+        self.location = location
+        self.age = age
+    }
+}
 
-var str = "Hello, playground"
+let linda = User(name: "Linda", location: "Park City", age: 53)
+
+linda.greeting()
+
+/*
+ 
+ Below, the call to the super initializer is the last statement. In Swift a subclass needs to set up all of its properties before calling the super initializer.
+ 
+ */
+ 
+ class Dragon: User {
+ let wingSpan: Int
+ 
+ init(name: String, location: String, age: Int, wingSpan: Int) {
+ self.wingSpan = wingSpan
+    super.init(name: name, location: location, age: age)
+    
+    //In Swift a subclass needs to set up all of its properties before calling the super initializer.
+
+ 
+ }
+ 
+ 
+ override func greeting() {
+ print("Argh ... me name is \(name) and my wings are \(wingSpan) feet long")
+ }
+ }
+ 
+ let puff = Dragon(name: "Puff", location: "Middle Earth", age: 350, wingSpan: 100)
+ 
+ puff.greeting()
+
+
+
+
+
+
