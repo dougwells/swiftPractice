21264043ded105eb7e4cf50babdd887730d03803
dougwells <dougwells@gmail.com>
2017-04-28 13:15:53 -0600

    Practice. Protocol extensions

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -11,37 +11,53 @@ Protocol notes:
 
 import UIKit
 
-protocol Vehicle {
+protocol Vehicle: CustomStringConvertible {
     var isRunning: Bool { get set }
+    var make: String { get set }
+    var model: String { get set }
     mutating func start()
     mutating func turnOff()
 }
 
-//Telling Swift that SportsCar will conform to Vehicle protocol
-struct SportsCar: Vehicle {
-    var isRunning: Bool = false
+//Extension makes functions & computed properties available to objects that conform to the protocol w/o each object adding it individually.
+extension Vehicle {
+    var makeModel: String {return "\(make) \(model)"}
+    
     mutating func start() {
         isRunning = true
-        print("Vroom ...")
+        print("\(self.description). Vroom ...")
     }
     mutating func turnOff() {
         isRunning = false
-        print("Good race")
+        print("\(self.description).  Nice ride!")
     }
 }
 
+//Telling Swift that SportsCar will conform to Vehicle protocol
+struct SportsCar: Vehicle, CustomStringConvertible {
+    var isRunning: Bool = false
+    var description: String { return self.makeModel }
+    var make: String
+    var model: String
+    
+
+
+    }
+
 //Protocols can also be used with reference types (classes).  Just saying it conforms to the prototype
 
-class Truck: Vehicle {
+class Truck: Vehicle, CustomStringConvertible {
     var isRunning: Bool = false
-    func start() {
-        isRunning = true
-        print("Big Truck is started")
+    var make: String
+    var model: String
+    
+    init(isRunning: Bool, make: String, model: String) {
+        self.isRunning = isRunning
+        self.make = make
+        self.model = model
     }
-    func turnOff() {
-        isRunning = false
-        print("Big Truck is off")
-    }
+    
+        var description: String { return self.makeModel }
     
     //Extend the protocol by adding a function/property not in the protocol (OK to have more than protocol requires)
     func blowHorn() {
@@ -50,8 +66,8 @@ class Truck: Vehicle {
 }
 
 
-var bmw = SportsCar()
-var peterbuilt = Truck()
+var bmw = SportsCar(isRunning: true, make: "BMW", model: "328xi")
+var peterbuilt = Truck(isRunning: true, make: "Peter", model: "Built")
 bmw.start()
 peterbuilt.start()
 
@@ -61,7 +77,19 @@ peterbuilt.turnOff()
 let vehicleArr: Array<Vehicle> = [bmw, peterbuilt]
 
 for vehicle in vehicleArr {
+    print (vehicle.description)
     if let veh = vehicle as? Truck {
         veh.blowHorn()
     }
 }
+
+//protocol extensions work similar to type extensions (see below). Once add an extension, all types get that added functionality
+
+extension Double {
+    var dollarString: String {
+        return String(format: "$%.02f", self)
+    }
+}
+
+var tax = 100 * 0.0825
+tax.dollarString
