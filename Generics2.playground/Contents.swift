90a9122b29a113f9e58a10c8d08f699951198754
dougwells <dougwells@gmail.com>
2017-04-30 08:29:15 -0600

    Practice. More generics. Using protocol extension to change binary operation of a Type.

--- Generics2.playground/Contents.swift
+++ Generics2.playground/Contents.swift
@@ -16,12 +16,38 @@ func addFloat(num: Float) -> Float {
 
 addFloat(num: 15.0)
 
-func addAny <T: Strideable>(num: T) -> T {
+func addAny <T: Strideable>(num: T) -> T {  //conforms to Strideable protocol
     return num + 1
 }
 
 addAny(num: 15)
 addAny(num: 15.0)
+
+func intMultiply (lhs: Int, rhs: Int) -> Int {
+    return lhs*rhs
+}
+
+intMultiply(lhs: 5, rhs: 3)
+
+func doubleMultiply(lhs: Double, rhs: Double) -> Double {
+    return lhs*rhs
+}
+
+doubleMultiply(lhs: 15.0, rhs: 3.0)
+
+protocol Numeric {
+    static func * (lhs: Self, rhs: Self) -> Self
+}
+
+extension Int: Numeric {}
+extension Double: Numeric {}
+
+func genericMultiply <T: Numeric>(lhs: T, rhs: T) -> T {
+    return lhs*rhs
+}
+
+genericMultiply(lhs: 3, rhs: 5)
+genericMultiply(lhs: 3.0, rhs: 5.0)
 
 
 
